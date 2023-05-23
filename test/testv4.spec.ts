c47647f8b360f53c1a7d157b0de0f742e3a9289e
umang93769 <42294165+umang93769@users.noreply.github.com>
2023-05-23 12:06:51 +0530

    Delete testv4.spec.ts

--- test/testv4.spec.ts
+++
@@ -1,9 +0,0 @@
-Scenario:
-
-Test Code: 
-describe('Button click test', function() {
-    it('should be clickable', function() {
-        var button = document.getElementById('button');
-        expect(button.click).toBeDefined();
-    });
-});
